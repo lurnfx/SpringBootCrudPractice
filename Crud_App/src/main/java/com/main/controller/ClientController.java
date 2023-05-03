12c2ef499dffeebfd8181975d758e54468fcb124
lurnfx <bhushanjawale80@gmail.com>
2023-05-03 16:44:11 +0530

    Updated Client Controller

--- Crud_App/src/main/java/com/main/controller/ClientController.java
+++ Crud_App/src/main/java/com/main/controller/ClientController.java
@@ -1,5 +1,18 @@
+package com.main.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ClientController
 {
+	
+	@GetMapping("")
+	public String msg(){
+		
+	   return "SucessFully Created And Return";
+		
+	}
 
 
 }
